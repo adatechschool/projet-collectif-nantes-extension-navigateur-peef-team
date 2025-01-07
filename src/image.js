009a575f401e29300cf3ea87f8addeb45ffa472c
Eloi RM <eloimailmaynad@gmail.com>
2025-01-07 16:05:18 +0100

    added : thumbnail changing nicely

--- src/image.js
+++ src/image.js
@@ -1,15 +1,16 @@
-const thumbnail = document.getElementsByTagName("img")
+// const thumbnail = document.getElementsByTagName("img")
 
 let myRacoons = []
+getRacoons()
 
-async function zaWorldo() {
-    myRacoons = await getRacoons()
-    changeThumbnail()
-    addEventListener('scroll', () => {
-        console.log("scroll detected!")
-        changeThumbnail()
-    })
-}
+// async function zaWorldo() {
+//     myRacoons = await getRacoons()
+//     changeThumbnail()
+//     addEventListener('scroll', () => {
+//         console.log("scroll detected!")
+//         changeThumbnail()
+//     })
+// }
 
 async function getRacoons() {
     try {
@@ -21,22 +22,78 @@ async function getRacoons() {
         
         if (data == undefined) {
             console.log("data is undefined")
-            return ["404.png"]
+            myRacoons = ["404.png"]
         } else {
             console.log("data is NOT undefined")
-            return data
+            myRacoons = data
         }
+        raccoon()
     } catch (error) {
         console.log(error)
     }
 }
 
-function changeThumbnail() {
-    console.log("changing thumbnail...")
-    for (const image of thumbnail) {
-        const i = Math.floor(Math.random() * myRacoons.length)
-        image.src = myRacoons[i].url
-    }
-}
+// function changeThumbnail() {
+//     console.log("changing thumbnail...")
+//     for (const image of thumbnail) {
+//         const i = Math.floor(Math.random() * myRacoons.length)
+//         image.src = myRacoons[i].url
+//     }
+// }
 
-zaWorldo()
+// zaWorldo()
+
+function raccoon()
+    {
+        var isEnabled = true;
+
+        //NOTE: The purpose of this function is to get all YouTube thumbnails on the page
+        function getThumbnails()
+        {
+            const thumbnailQuery = "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])";
+
+            const thumbnail = document.querySelectorAll(thumbnailQuery);
+
+            thumbnail.forEach((image) =>
+                {
+                    let counter = Math.random() > 0.001 ? 1 : 20;
+                    let i = 0;
+                    for(i = 0; i < counter; i++)
+                    {
+                        let url = myRacoons[Math.floor(Math.random() * myRacoons.length)].url
+                        applyThumbnails(image, url);
+                    }
+                }
+            )
+        }
+
+        //NOTE: The purpose of this function is to apply the thumbnail images to the thumbnails on YouTube.com
+        function applyThumbnails(image, imageUrl)
+        {
+            if (image.nodeName == "IMG")
+            {``
+                // image.src = myRacoons[Math.floor(Math.random() * myRacoons.length)]
+                const overlay = document.createElement("img");
+                overlay.src = imageUrl;
+                overlay.style.position = "absolute";
+                overlay.style.top = "0";
+                overlay.style.left = "0";
+                overlay.style.width = "100%";
+                overlay.style.height = "100%";
+                overlay.style.zIndex = "0";
+
+                image.style.position = "relative";
+                image.parentElement.appendChild(overlay);
+            }
+            else if (image.nodeName == "DIV")
+            {
+                image.style.backgroundImage = `url("${imageUrl}"), ` + image.style.backgroundImage;
+            }
+        }
+
+        //runs the functions
+        if(isEnabled) //checks if the user has disabled the plugin or not
+        {
+            setInterval(getThumbnails, 100);
+        }
+    }

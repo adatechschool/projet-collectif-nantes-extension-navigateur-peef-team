30a03876f60cea194e0200e0084c68fd72899d71
Eloi RM <eloimailmaynad@gmail.com>
2025-01-07 14:17:26 +0100

    added : manifest and base script

---
+++ src/image.js
@@ -0,0 +1,37 @@
+const imgs = document.getElementsByTagName("img")
+
+let myRacoons = []
+
+async function zaWorldo() {
+    myRacoons = await getRacoons()
+    changeThumbnail()
+}
+
+async function getRacoons() {
+    try {
+        const response = await fetch("https://api.racc.lol/v1/raccoons?take=50&random=true")
+        const json = await response.json()
+    
+        const data = json.data
+        console.log(data)
+        
+        if (data == undefined) {
+            console.log("data is undefined")
+            return ["404.png"]
+        } else {
+            console.log("data is NOT undefined")
+            return data
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function changeThumbnail() {
+    for (const image of imgs) {
+        const i = Math.floor(Math.random() * myRacoons.length)
+        image.src = myRacoons[i].url
+    }
+}
+
+zaWorldo()

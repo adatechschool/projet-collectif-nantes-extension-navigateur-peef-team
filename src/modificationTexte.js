a9665edda2b34c6142eb61ba8e5b87fb3df25c2c
flo <boireaufloriane@hotmail.fr>
2025-01-14 15:13:08 +0100

    "#"

--- src/modificationTexte.js
+++ src/modificationTexte.js
@@ -10,8 +10,10 @@
 //     })
 // })
 
+// Permet de rajouter devant chaque texte le mot racoon 
 function changeWords() {
 
+    // Balise YouTube ciblées 
     const elements = document.querySelectorAll("yt-formatted-string") 
         
     if(elements) {
@@ -24,4 +26,5 @@ function changeWords() {
     }
 }
 
+// Permet d'attendre que YouTube soit charger 
 setTimeout(changeWords, 1000)
